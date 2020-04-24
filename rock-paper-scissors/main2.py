5bfcea5a8cd2fd59b2337e96c4ef30cc1f9b195d
as-segaf <ziyanassegaf20@gmail.com>
2020-04-24 09:17:27 +0700

    rock paper scissor

---
+++ rock-paper-scissors/main2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3.8
+
+import random
+
+print("Hello! What is your name?")
+username = input()
+print(f"Well, {username}, let's play rock-paper-scissor game\nThe game ends when one of us get 3 wins")
+print("Press 1 to choose rock, press 2 to choose paper, press 3 for scissor")
+options = {1:'rock',2:'paper',3:'scissor'}
+com_wins, user_wins = 0,0
+
+while com_wins < 3 and user_wins < 3: 
+    com_pick = options[random.randint(1,3)] 
+    pick_num = int(input("Pick yours:"))
+    user_pick = options[pick_num]
+    print(f"{user_pick}(you) vs {com_pick}(com)")
+
+    if comp_pick == user_pick:
+        print("Draw")
+    elif comp_pick == 'rock' and user_pick == 'scissor':
+        print("Computer wins")
+        com_wins+=1
+    elif comp_pick == 'rock' and user_pick == 'paper':
+        print("You win")
+        user_wins+=1
+    elif comp_pick == 'paper' and user_pick == 'rock':
+        print("Computer wins")
+        com_wins+=1
+    elif comp_pick == 'paper' and user_pick == 'scissor':
+        print("You win")
+        user_wins+=1
+    elif comp_pick == 'scissor' and user_pick == 'paper':
+        print("Computer wins")
+        com_wins+=1
+    elif comp_pick == 'scissor' and user_pick == 'rock':
+        print("You win")
+        user_wins+=1
+
+    print("Computer's wins:",com_wins)
+    print("Your wins:",user_wins)
---
+++ rock-paper-scissors/readme.md
@@ -0,0 +1,3 @@
+## Rock-paper-scissor
+
+A simple rock-paper-scissor game where it is the user vs the computer. The computer's pick will be randomly generated, while the program will ask the user for their input. The game ends when one of them get 3 wins
