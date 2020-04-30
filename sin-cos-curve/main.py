cc1b5d1004b769c9cfee0af44bc250c71b5c3bfa
as-segaf <ziyanassegaf20@gmail.com>
2020-04-30 14:25:48 +0700

    sin cos curve

---
+++ sin-cos-curve/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.8
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+degree = np.arange(-360,361)
+x = np.arange(-360,361)
+y = np.sin(degree * np.pi/180)
+z = np.cos(degree * np.pi/180)
+plt.plot(x,y,x,z)
+plt.xlabel("degree")
+plt.ylabel("value")
+plt.title("Sin Cos Curve")
+plt.legend(['sin(x)', 'cos(x)'])
+plt.show()
+
+# x = np.arange(-360,361)
+# y = np.cos(x * np.pi/180)
+# plt.plot(x,y)
+# plt.xlabel("degree")
+# plt.ylabel("value")
+# plt.title("Cos Curve")
+# plt.show()
